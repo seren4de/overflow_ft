87293fb1fad7c918d08af890e5f74da145c716be
Bit-nub <a.khalilelhassar@gmail.com>
2023-07-10 10:50:06 +0100

    Update test.sh

--- test.sh
+++ test.sh
@@ -11,7 +11,7 @@ out_path="./audio/output_audio/testlr/"
 model_path="/home/hannibal/overflow_ft/out/"
 
 #tested checkpoints
-cps=("21000" "21500")
+cps=("21000" "21500" "22000")
 
 #tested learning rates  "1e-2"
 lrs=("1e-5" "1e-5-2" "1e-4" "1e-3")
@@ -22,6 +22,6 @@ do
     for cp in "${cps[@]}";
     do
         echo "[+] Testing the model with the learning rate $lr and the checkpoint $cp"
-        tts --text "$test_phrase" --model_path $model_path$lr"/checkpoint_"$cp".pth" --config_path $model_path$lr"/config.json" --out_path $out_path$lr$cp".wav" --vocoder_name $vocoder_name
+        tts --text "$test_phrase" --model_path $model_path$lr"/checkpoint_"$cp".pth" --config_path $model_path$lr"/config.json" --out_path $out_path$lr$cp".wav" --vocoder_name $vocoder_name 2>/dev/null
     done
 done
